ff01276f6cd8f66c12255278848aacc39f280925
Fatima Oday <fatmaoa98@gmail.com>
2021-03-16 22:48:09 +0300

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  imagebaseurl: "/",
+  name: "Fatima",
+  fullName: "Fatima Oday Ahmed",
+  role: "Web developer | UI/UX Designer",
+  roleDescription:
+    "I'm a junior web developer and user interface designer. I'm very passionate about learning more about programming and gaining more experience. I'm also interested in business and entrepreneurship.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/fatima-oday-3a713b167/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/fatimaoday/",
+      className: "fa fa-github",
+    },
+    {
+      name: "facebook",
+      url: "https://www.facebook.com/fatma.oday.1",
+      className: "fa fa-facebook",
+    },
+  ],
+  aboutme:
+    "I'm a recent graduate with a great passion for self-development and programming. I spend a lot of my time learning new skills online or through attending workshops and events. I also love to volunteer in social activities and community service.",
+  address: "Baghdad, Iraq",
+  email: "fatmaoa98@gmail.com",
+  education: [
+    {
+      UniversityName: "University of Technology",
+      specialization: "Computer Science",
+      MonthOfPassing: "Sep",
+      YearOfPassing: "2020",
+      Achievements: "Ranked 2nd in the class of 2020",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Freelancer",
+      specialization: "Web Development",
+      MonthOfLeaving: "Until now",
+    },
+    {
+      CompanyName: "Master Group Technology",
+      specialization: "Technician & Customer Service",
+      MonthOfLeaving: "March",
+      YearOfLeaving: "2021",
+      Achievements: "Internship",
+    },
+  ],
+  skillsDescription: "I Am Good With",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "reactjs",
+    },
+    {
+      skillname: "nodejs",
+    },
+    {
+      skillname: "adobexd",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Face Detection",
+      description: "Face Detection with OpenCV",
+      imgurl: "images/portfolio/project1.jpg",
+      url: "https://github.com/fatimaoday/Face-Recognition-with-OpenCV",
+    },
+    {
+      name: "Fashion Clothes Classification",
+      description:
+        "Deep Learning CNN for Fashion-MNIST Clothing Classification",
+      imgurl: "images/portfolio/project2.jpg",
+      url: "https://github.com/fatimaoday/CNN-with-Fashion-MNIST-dataset",
+    },
+    {
+      name: "Face Mask Detection",
+      description:
+        "Face Mask Detection using Convolutional Neural Networks with Python, Keras, Tensorflow and OpenCV.",
+      imgurl: "images/portfolio/project3.jpg",
+      url: "https://github.com/fatimaoday/Face-mask-detection-using-CNN",
+    },
+    {
+      name: "Speech Recognition",
+      description: "Simple Speech Recognition App",
+      imgurl: "images/portfolio/project4.jpg",
+      url:
+        "https://gist.github.com/fatimaoday/43158f769de13398595d4541d4d7f581",
+    },
+  ],
+  testimonials: [
+    {
+      description: "Miracle is another name for hard work.",
+      name: "Unknown",
+    }
+  ],
+};
+export default resumeData;
